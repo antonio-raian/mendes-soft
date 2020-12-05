c718ffdd25eb77dbfe2517e3045657e8c92f3cfe
Antonio pc DevoluTI <raymendesjr2013@gmail.com>
2020-12-04 21:37:25 -0300

    add: Itens by category on dashboard

--- mendes-soft-web/src/pages/DashBoard/index.tsx
+++ mendes-soft-web/src/pages/DashBoard/index.tsx
@@ -1,12 +1,62 @@
+import Loading from "@/components/Loading";
+import { useAuth } from "@/hooks/auth";
+import { Category } from "@/interfaces";
 import DashboardLayout from "@/layouts/DashLayout";
-import React from "react";
+import api from "@/services/api";
+import React, { ReactText, useEffect, useState } from "react";
+import Chart from "react-google-charts";
+import { useHistory } from "react-router-dom";
 import { Container } from "./styles";
 
 const DashBoard = () => {
+  const history = useHistory();
+  const { signOut } = useAuth();
+
+  const [dataCategory, setDataCategory] = useState<ReactText[][]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function handleLoad() {
+      await api
+        .get<Category[]>("/category")
+        .then((response) => {
+          const aux: ReactText[][] = [["Category", "Itens"]];
+          response.data.map((cat) => {
+            aux.push([cat.name, cat.items.length]);
+          });
+          setDataCategory(aux);
+          setLoading(false);
+          console.log(aux);
+        })
+        .catch((e) => {
+          console.log(e.response);
+          if (e.response?.status === 401) {
+            signOut();
+            history.goBack();
+          }
+        });
+    }
+    handleLoad();
+  }, []);
+
   return (
     <DashboardLayout>
       <Container>
-        <h1>DASHBOARD</h1>
+        {loading ? (
+          <Loading />
+        ) : (
+          <Chart
+            chartType="PieChart"
+            width={"500px"}
+            height={"500px"}
+            data={dataCategory}
+            options={{
+              legend: "none",
+              pieSliceText: "label",
+              title: "Quantidade de Produtos por Categoria",
+            }}
+          />
+        )}
       </Container>
     </DashboardLayout>
   );

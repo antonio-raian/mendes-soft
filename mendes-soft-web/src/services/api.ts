f273b91cbc3ab76eba6f3e8a41c731eadcb010fc
Antonio pc DevoluTI <raymendesjr2013@gmail.com>
2021-01-13 11:48:46 -0300

    merge

--- mendes-soft-web/src/services/api.ts
+++ mendes-soft-web/src/services/api.ts
@@ -1,7 +1,8 @@
 import Axios from "axios";
 
-// const urlServer = "http://192.168.0.108:8080";
-const urlServer = "http://34.122.241.243:8080";
+// const urlServer = "http://221.1.1.7:8080";
+const urlServer = "http://192.168.0.104:8080";
+// const urlServer = "http://34.122.241.243:8080";
 
 const api = Axios.create({
   baseURL: urlServer,

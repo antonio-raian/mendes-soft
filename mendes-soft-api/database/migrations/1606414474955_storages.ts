a64840ca4777a94bde984001eb637b059b67d52b
Antonio Raian <raymendesjr2013@gmail.com>
2021-01-13 11:24:22 -0300

    feat: Add unique key to item on storages table

--- mendes-soft-api/database/migrations/1606414474955_storages.ts
+++ mendes-soft-api/database/migrations/1606414474955_storages.ts
@@ -8,13 +8,21 @@ export default class Storages extends BaseSchema {
       this.schema.createTable(this.tableName, (table) => {
         table.bigIncrements("id");
 
-        table.bigInteger("item_id").references("items.id").notNullable();
+        table
+          .bigInteger("item_id")
+          .references("items.id")
+          .unique()
+          .notNullable();
         table.float("quantity");
         table.float("value_sale");
         table.float("value_cost");
         table.boolean("active").defaultTo(true);
 
         table.timestamps(true);
+      });
+    else
+      this.schema.alterTable(this.tableName, (table) => {
+        table.bigInteger("item_id").notNullable().unique().alter();
       });
   }
 

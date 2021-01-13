fc5568d42e4dbdf153c1f3e16e8652736f713236
Antonio pc DevoluTI <raymendesjr2013@gmail.com>
2021-01-13 11:50:02 -0300

    fix: Api URLServer

--- mendes-soft-web/src/services/api.ts
+++ mendes-soft-web/src/services/api.ts
@@ -1,8 +1,7 @@
 import Axios from "axios";
 
-// const urlServer = "http://221.1.1.7:8080";
-const urlServer = "http://192.168.0.104:8080";
-// const urlServer = "http://34.122.241.243:8080";
+// const urlServer = "http://192.168.0.104:8080";
+const urlServer = "http://34.122.241.243:8080";
 
 const api = Axios.create({
   baseURL: urlServer,
